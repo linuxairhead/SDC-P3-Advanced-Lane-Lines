b0c5936d5cd4b72946e812efcabecfb5f1e71a01
Chang Ki Hong <c_chanho@quicinc.com>
2018-05-30 01:01:47 -0700

    Gradjent and Direction of the Magnitude

--- Utility.py
+++ Utility.py
@@ -16,7 +16,7 @@ def warpPoint(img):
 	
     # define source and destination points for transform
     src = np.float32([(575,464), (707,464), (258,682), (1049,682)])	
-    dst = np.float32([(450,0), (width-450,0), (450,hight), (width-450,hight)])
+    dst = np.float32([(200,0), (width-200,0), (200,hight), (width-200,hight)])
 	
     return src, dst, hight, width
 	
@@ -36,11 +36,11 @@ def unwarp(img):
 # then takes an absolute value and applies a threshold.
 # Note: calling your function with orient='x', thresh_min=5, thresh_max=100
 # should produce output like the example image shown above this quiz.
-def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
+def abs_sobel_thresh(img, orient='x', thresh_min=25, thresh_max=255):
     
     # Apply the following steps to img
     # 1) Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY )
     
     # 2) Take the derivative in x or y given orient = 'x' or 'y'
     # 3) Take the absolute value of the derivative or gradient    
@@ -59,15 +59,81 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     # 6) Return this mask as your binary_output image
     
     return binary_output
+
 	
-def displayResult( img1, img2, title1, title2 ):
+# Define a function that applies Sobel x and y, 
+# then computes the magnitude of the gradient
+# and applies a threshold
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude 
+    gradmag = np.sqrt(np.square(sobelx) + np.square(sobely))
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scale_factor = np.max(gradmag)/255 
+    gradmag = (gradmag/scale_factor).astype(np.uint8)     
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1    
+    # 6) Return this mask as your binary_output image
+
+    return binary_output
+
+	
+# Define a function that applies Sobel x and y, 
+# then computes the direction of the gradient
+# and applies a threshold.
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction, 
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    binary_output =  np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
+	
+	
+# Define a function that thresholds the S-channel of HLS
+# Use exclusive lower bound (>) and inclusive upper (<=)
+def hls_select(img, thresh=(125, 255)):
+    # 1) Convert to HLS color space
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    
+    # 2) Apply a threshold to the S channel
+    s_channel = hls[:,:,2]
+    binary_output = np.zeros_like(s_channel)
+    binary_output[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
+    
+    # 3) Return a binary image of threshold result
+    return binary_output
+	
+def displayResult( img1, img2, title1, title2, withUnWarp = False ):
 
 	fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
 	fig.subplots_adjust(hspace = .2, wspace=.05)
 
 	ax1.imshow(img1)
+	if withUnWarp == True:
+		
+		src, dst, h, w = warpPoint(img1)
+	
+		x = [src[0][0],src[2][0],src[3][0],src[1][0],src[0][0]]
+		y = [src[0][1],src[2][1],src[3][1],src[1][1],src[0][1]]
+		ax1.plot(x, y, color='#33cc99', alpha=0.4, linewidth=3, solid_capstyle='round', zorder=2)	
+	
 	ax1.set_title(title1, fontsize=20)
 
-	ax2.imshow(img2)
+	ax2.imshow(img2,  cmap='gray')
 	ax2.set_title(title2, fontsize=20)
 	
